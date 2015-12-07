29dc02b077cb69f4c3a819674d8383391b6c5356
Neil <neil@Neils-MacBook-Pro.local>
2015-12-07 12:09:02 -0800

    Adding Ruby-RSpec-Capybara test framework

---
+++ Gemfile
@@ -0,0 +1,10 @@
+
+source "https://www.rubygems.org"
+
+gem 'rspec'
+gem 'sauce_whisk'
+gem 'capybara'
+gem 'parallel_tests'
+gem 'require_all'
+gem 'selenium-webdriver'
+gem 'rake'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,46 @@
+require "sauce_whisk"
+require "capybara/rspec"
+require "capybara"
+require "rspec"
+require "json"
+
+Capybara.register_driver :selenium do | app|
+  capabilities = {
+    :version => ENV['version'],
+    :browserName => ENV['browserName'],
+    :platform => ENV['platform'],
+  }
+
+  url = "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub".strip
+
+  Capybara::Selenium::Driver.new(app,
+                                 :browser => :remote, :url => url,
+                                 :desired_capabilities => capabilities)
+end
+
+Capybara.default_max_wait_time = 10
+Capybara.current_driver = :selenium
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+  config.include Capybara::RSpecMatchers
+
+  config.after(:each) do | scenario |
+    jobname = scenario.full_description
+    sessionid = ::Capybara.current_session.driver.browser.session_id
+    ::Capybara.current_session.driver.quit
+
+    # Output sessionId and jobname to std out for Sauce OnDemand Plugin to display embeded results
+    puts "SauceOnDemandSessionID=#{sessionid} job-name=#{jobname}"
+
+    job = SauceWhisk::Jobs.fetch sessionid
+    job.name = jobname
+    job.save
+
+    if scenario.exception
+      SauceWhisk::Jobs.fail_job sessionid
+    else
+      SauceWhisk::Jobs.pass_job sessionid
+    end
+  end
+end
