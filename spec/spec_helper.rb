f8a9862d7f5efc38278eabfd3b32cf2333896f85
ndmanvar <ndmanvar@yahoo.com>
2016-10-12 17:19:55 -0700

    using https

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ RSpec.configure do | config |
       :name => x.full_description
     }
 
-    url = "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub".strip
+    url = "https://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:443/wd/hub".strip
 
     client = Selenium::WebDriver::Remote::Http::Default.new
     client.timeout = 180
