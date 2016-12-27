4c9a1fb001459173e398c051d633fcc527b4d7b2
Titus Fortner <titusfortner@gmail.com>
2016-12-26 22:30:04 -0600

    update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,50 +1,44 @@
-require "sauce_whisk"
-require "capybara/rspec"
 require "capybara"
+require "capybara/rspec"
 require "rspec"
-require "json"
+require "sauce_whisk"
+
 
 Capybara.default_max_wait_time = 10
-Capybara.default_driver = :selenium
 
 RSpec.configure do |config|
   config.include Capybara::DSL
   config.include Capybara::RSpecMatchers
 
-  config.before(:each) do | scenario |
-    Capybara.register_driver :selenium do | app|
+  config.before(:each) do |example|
+    Capybara.register_driver :selenium do |app|
+      build_name = ENV['JENKINS_BUILD_NUMBER'] ||
+          ENV['SAUCE_BAMBOO_BUILDNUMBER'] ||
+          ENV['SAUCE_TC_BUILDNUMBER'] ||
+          ENV['SAUCE_BUILD_NAME'] ||
+          'LOCAL'
+
       capabilities = {
-        :version => ENV['version'],
-        :browserName => ENV['browserName'],
-        :platform => ENV['platform'],
-        :name => scenario.full_description
+          version: ENV['version'],
+          browserName: ENV['browserName'],
+          platform: ENV['platform'],
+          name: example.description,
+          build: build_name
       }
       url = "https://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:443/wd/hub".strip
 
-      Capybara::Selenium::Driver.new(app,
-                                     :browser => :remote, :url => url,
-                                     :desired_capabilities => capabilities)
+      Capybara::Selenium::Driver.new(app, {browser: :remote,
+                                           url: url,
+                                           desired_capabilities: capabilities})
     end
-
-    # Capybara.current_driver = :remote
-    jobname = scenario.full_description
-    Capybara.session_name = "#{jobname} - #{ENV['platform']} - " +
-      "#{ENV['browserName']} - #{ENV['version']}"
-
-    @driver = Capybara.current_session.driver
-
-    # Output sessionId and jobname to std out for Sauce OnDemand Plugin to display embeded results
-    @session_id = @driver.browser.session_id
-    puts "SauceOnDemandSessionID=#{@session_id} job-name=#{jobname}"
+    Capybara.current_driver = :selenium
   end
 
-  config.after(:each) do | scenario |
-    @driver.quit
-    Capybara.use_default_driver
-    if scenario.exception
-      SauceWhisk::Jobs.fail_job @session_id
-    else
-      SauceWhisk::Jobs.pass_job @session_id
-    end
+
+  config.after(:each) do |example|
+    session_id = ::Capybara.current_session.driver.browser.session_id
+    ::Capybara.current_session.driver.quit
+
+    SauceWhisk::Jobs.change_status(session_id, example.exception.nil?)
   end
 end
