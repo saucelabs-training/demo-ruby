05f26fcea8bcf85e8db3af6a4721d634698e9cbc
Titus Fortner <titusfortner@gmail.com>
2018-11-15 20:38:48 -0600

    its options not capabilities

--- features/support/sauce_helpers.rb
+++ features/support/sauce_helpers.rb
@@ -2,18 +2,18 @@ require 'sauce_whisk'
 
 module SauceHelpers
   def initialize_browser(name)
-    capabilities = {name: name,
+    opt = {name: name,
                     build: build_name}
 
     YAML.safe_load(IO.read('features/support/platforms.yml'))[platform].each do |key, value|
-      capabilities[key] = value
+      opt[key] = value
     end
 
-    capabilities[:url] = "https://ondemand.saucelabs.com:443/wd/hub"
-    capabilities[:username] = ENV['SAUCE_USERNAME']
-    capabilities[:accessKey] = ENV['SAUCE_ACCESS_KEY']
+    opt[:url] = "https://ondemand.saucelabs.com:443/wd/hub"
+    opt[:username] = ENV['SAUCE_USERNAME']
+    opt[:accessKey] = ENV['SAUCE_ACCESS_KEY']
 
-    @browser = Watir::Browser.new capabilities.delete('browser_name'), capabilities
+    @browser = Watir::Browser.new opt.delete('browser_name'), opt
   end
 
   def submit_results(session_id, result)
@@ -28,6 +28,6 @@ module SauceHelpers
     %w[SAUCE_BAMBOO_BUILDNUMBER CIRCLE_BUILD_NUM BUILD_TAG TRAVIS_BUILD_NUMBER"].each do |var|
       return ENV[var] if ENV.key?(var)
     end
-     "Unknown Build - #{Time.now.to_i}"
+    "Unknown Build - #{Time.now.to_i}"
   end
 end
