65102af7514e56bdfd3e57fcfff5650d28475db3
Titus Fortner <titusfortner@gmail.com>
2018-11-28 14:30:47 -0800

    update to standard template

--- spec/add_to_cart_spec.rb
+++ spec/add_to_cart_spec.rb
@@ -4,11 +4,6 @@ describe "Cart" do
   before { visit 'http://www.saucedemo.com/inventory.html'}
 
   it "adds one" do
-    # find('.inventory_item:first-child').click_button('ADD TO CART')
-    # click_button('ADD TO CART', match: :first)
-    # click_button(class: 'add-to-cart-button', match: :first)
-    # first(:button, 'ADD TO CART').click
-    # first(:button, class: ['add-to-cart-button']).click
     first('.add-to-cart-button').click
 
     expect(page).to have_css('.shopping_cart_badge', exact_text: '1')
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,15 +10,53 @@ Capybara.default_max_wait_time = 10
 RSpec.configure do |config|
   config.include Capybara::DSL
   config.include Capybara::RSpecMatchers
-  config.include SauceHelpers
+
+  def platforms
+    YAML.safe_load(IO.read('spec/platforms.yml'))
+  end
+
+  def platform
+    platforms[ENV['PLATFORM']] || platforms.first
+  end
+
+  def build_name
+    if ENV['BUILD_NAME']
+      ENV['BUILD_NAME']
+    elsif ENV['TRAVIS_JOB_NUMBER']
+      "#{ENV['TRAVIS_REPO_SLUG'][/[^\/]+$/]}: #{ENV['TRAVIS_JOB_NUMBER']}"
+    elsif ENV['SAUCE_BAMBOO_BUILDNUMBER']
+      ENV['SAUCE_BAMBOO_BUILDNUMBER']
+    elsif ENV['CIRCLE_BUILD_NUM']
+      "#{ENV['CIRCLE_JOB']}: #{ENV['CIRCLE_BUILD_NUM']}"
+    elsif ENV['BUILD_TAG']
+      ENV['BUILD_TAG']
+    else
+      "Local Execution - #{Time.now.to_i}"
+    end
+  end
 
   config.before(:each) do |example|
-    initialize_browser(example.description)
+    Capybara.register_driver :sauce do |app|
+      opt = {name: example.description,
+             build: build_name,
+             username: ENV['SAUCE_USERNAME'],
+             accessKey: ENV['SAUCE_ACCESS_KEY']}
+
+      opt.merge! YAML.safe_load(IO.read('spec/support/platforms.yml'))[platform]
+
+      caps = Selenium::WebDriver::Remote::Capabilities.send(opt.delete('browser_name').to_sym, opt)
+
+      url = 'https://ondemand.saucelabs.com:443/wd/hub'
+
+      @driver = Capybara::Selenium::Driver.new(app, {browser: :remote,
+                                                     url: url,
+                                                     desired_capabilities: caps})
+    end
+    Capybara.current_driver = :sauce
   end
 
   config.after(:each) do |example|
-    session_id = @driver.browser.session_id
-    submit_results(session_id, !example.exception)
+    SauceWhisk::Jobs.change_status(@driver.browser.session_id, !example.exception)
     @driver.quit
   end
 end
