93455a7e165a96c6d4292cc04f0df10db774e0ec
Titus <titusfortner@users.noreply.github.com>
2018-11-28 18:48:15 -0800

    Add build name method

--- features/support/env.rb
+++ features/support/env.rb
@@ -11,6 +11,22 @@ Before do |scenario|
     platforms[ENV['PLATFORM']] || platforms.first
   end
 
+  def build_name
+    if ENV['BUILD_NAME']
+      ENV['BUILD_NAME']
+    elsif ENV['TRAVIS_JOB_NUMBER']
+      "#{ENV['TRAVIS_REPO_SLUG'][/[^\/]+$/]}: #{ENV['TRAVIS_JOB_NUMBER']}"
+    elsif ENV['SAUCE_BAMBOO_BUILDNUMBER']
+      ENV['SAUCE_BAMBOO_BUILDNUMBER']
+    elsif ENV['CIRCLE_BUILD_NUM']
+      "#{ENV['CIRCLE_JOB']}: #{ENV['CIRCLE_BUILD_NUM']}"
+    elsif ENV['BUILD_TAG']
+      ENV['BUILD_TAG']
+    else
+      "Local Execution - #{Time.now.to_i}"
+    end
+  end
+
   opt = {name: "#{scenario.feature.name} - #{scenario.name}",
          build: build_name,
          url: "https://ondemand.saucelabs.com:443/wd/hub",
