95a06f7302ba8169f30f939d7d78aeab22bdd3e7
Titus Fortner <titusfortner@gmail.com>
2019-04-12 16:55:37 -0500

    update spec helper information

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ require "watir"
 require 'sauce_whisk'
 
 RSpec.configure do |config|
-  config.before(:each) do |test|
-    options = platform(test.full_description)
+  config.before(:each) do |example|
+    options = platform(example.full_description)
     options[:url] = 'https://ondemand.saucelabs.com:443/wd/hub'
 
     browser = options.delete(:browser_name)
@@ -21,29 +21,26 @@ RSpec.configure do |config|
   # Note that having this as a conditional in the test code is less ideal
   # It is better for static data to be pulled from a serialized file like a yaml
   #
+  # Note: not all browsers are defaulting to using w3c protocol
+  # This will change soon. Where possible prefer the w3c approach
+  #
   def platform(name)
     case ENV['PLATFORM']
     when 'windows_10_edge'
-      {selenium_version: '3.141.59',
-       platform_name: 'Windows 10',
+      {platform_name: 'Windows 10',
        browser_name: 'edge',
        browser_version: '18.17763'}.merge(sauce_w3c(name))
     when 'windows_8_ie'
       # Note: w3c is not working for Windows 8 & IE 11
       {platform: 'Windows 8.1',
        browser_name: 'ie',
-       selenium_version: '3.141.59',
        version: '11.0'}.merge(sauce_oss(name))
     when 'mac_sierra_chrome'
-      # This is for running with w3c which is not yet the default
-      #   {platform_name: 'macOS 10.12',
-      #    browser_name: 'chrome',
-      #    "goog:chromeOptions": {w3c: true},
-      #    browser_version: '65.0'}.merge(sauce_w3c name)
-      {selenium_version: '3.141.59',
-       platform: 'macOS 10.12',
+      # This is for running Chrome with w3c which is not yet the default
+      {platform_name: 'macOS 10.12',
        browser_name: 'chrome',
-       version: '65.0'}.merge(sauce_oss(name))
+       "goog:chromeOptions": {w3c: true},
+       browser_version: '65.0'}.merge(sauce_w3c(name))
     when 'mac_mojave_safari'
       {platform_name: 'macOS 10.14',
        browser_name: 'safari',
@@ -53,25 +50,28 @@ RSpec.configure do |config|
        browser_name: 'firefox',
        browser_version: '60.0'}.merge(sauce_w3c(name))
     else
-      # Always specify a default
-      {selenium_version: '3.141.59',
-       platform: 'macOS 10.12',
+      # Always specify a default;
+      # this doesn't force Chrome to w3c
+      {platform: 'macOS 10.12',
        browser_name: 'chrome',
        version: '65.0'}.merge(sauce_oss(name))
     end
   end
 
   def sauce_w3c(name)
-    w3c_opts = sauce_oss(name)
-    w3c_opts[:selenium_version] = '3.141.59'
-    {'sauce:options' => w3c_opts}
+    {'sauce:options' => {name: name,
+                         build: build_name,
+                         username: ENV['SAUCE_USERNAME'],
+                         access_key: ENV['SAUCE_ACCESS_KEY'],
+                         selenium_version: '3.141.59'}}
   end
 
   def sauce_oss(name)
     {name: name,
      build: build_name,
      username: ENV['SAUCE_USERNAME'],
-     access_key: ENV['SAUCE_ACCESS_KEY']}
+     access_key: ENV['SAUCE_ACCESS_KEY'],
+     selenium_version: '3.141.59'}
   end
 
   #
@@ -80,7 +80,7 @@ RSpec.configure do |config|
   #
   def build_name
     if ENV['TRAVIS_REPO_SLUG']
-      "#{ENV['TRAVIS_REPO_SLUG'][/[^\/]+$/]}: #{ENV['TRAVIS_JOB_NUMBER']}"
+      "#{ENV['TRAVIS_REPO_SLUG'][%r{[^/]+$}]}: #{ENV['TRAVIS_JOB_NUMBER']}"
     elsif ENV['SAUCE_START_TIME']
       ENV['SAUCE_START_TIME']
     else
