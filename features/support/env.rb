afc84ad50d49fe2e6824cf97332a7dbbf5e25b6f
Titus Fortner <titusfortner@gmail.com>
2019-04-12 11:50:02 -0500

    update how platforms get referenced

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,52 +1,96 @@
 require 'capybara/cucumber'
 require 'selenium-webdriver'
-require 'sauce_whisk'
-
-def platforms
-  YAML.safe_load(IO.read('features/support/platforms.yml'))
-end
-
-def platform
-  platforms[ENV['PLATFORM']] || platforms.first
-end
-
-def build_name
-  if ENV['BUILD_NAME']
-    ENV['BUILD_NAME']
-  elsif ENV['TRAVIS_JOB_NUMBER']
-    "#{ENV['TRAVIS_REPO_SLUG'][/[^\/]+$/]}: #{ENV['TRAVIS_JOB_NUMBER']}"
-  elsif ENV['SAUCE_BAMBOO_BUILDNUMBER']
-    ENV['SAUCE_BAMBOO_BUILDNUMBER']
-  elsif ENV['CIRCLE_BUILD_NUM']
-    "#{ENV['CIRCLE_JOB']}: #{ENV['CIRCLE_BUILD_NUM']}"
-  elsif ENV['BUILD_TAG']
-    ENV['BUILD_TAG']
-  else
-    "Local Execution - #{Time.now.to_i}"
-  end
-end
+require "sauce_whisk"
 
 Before do |scenario|
   Capybara.register_driver :sauce do |app|
-    opt = {name: "#{scenario.feature.name} - #{scenario.name}",
-           build: build_name,
-           username: ENV['SAUCE_USERNAME'],
-           accessKey: ENV['SAUCE_ACCESS_KEY']}
+    opt = platform("#{scenario.feature.name} - #{scenario.name}")
 
-    opt.merge! platform
-
-    caps = Selenium::WebDriver::Remote::Capabilities.send(opt.delete('browser_name').to_sym, opt)
+    caps = Selenium::WebDriver::Remote::Capabilities.send(opt.delete(:browser_name).to_sym, opt)
 
     url = 'https://ondemand.saucelabs.com:443/wd/hub'
 
-    @driver = Capybara::Selenium::Driver.new(app, {browser: :remote,
-                                                   url: url,
-                                                   desired_capabilities: caps})
+    Capybara::Selenium::Driver.new(app, {browser: :remote,
+                                         url: url,
+                                         desired_capabilities: caps})
   end
   Capybara.current_driver = :sauce
 end
 
 After do |scenario|
-  SauceWhisk::Jobs.change_status(@driver.browser.session_id, scenario.passed?)
-  @driver.quit
+  session_id = Capybara.current_session.driver.browser.session_id
+  SauceWhisk::Jobs.change_status(session_id, scenario.passed?)
+  Capybara.current_session.quit
+end
+
+#
+# Note that having this as a conditional in the test code is less ideal
+# It is better for static data to be pulled from a serialized file like a yaml
+#
+# Note: not all browsers are defaulting to using w3c protocol
+# This will change soon. Where possible prefer the w3c approach
+#
+def platform(name)
+  case ENV['PLATFORM']
+  when 'windows_10_edge'
+    {platform_name: 'Windows 10',
+     browser_name: 'edge',
+     browser_version: '18.17763'}.merge(sauce_w3c(name))
+  when 'windows_8_ie'
+    # Note: w3c is not working for Windows 8 & IE 11
+    {platform: 'Windows 8.1',
+     browser_name: 'ie',
+     version: '11.0'}.merge(sauce_oss(name))
+  when 'mac_sierra_chrome'
+    # This is for running Chrome with w3c which is not yet the default
+    {platform_name: 'macOS 10.12',
+     browser_name: 'chrome',
+     "goog:chromeOptions": {w3c: true},
+     browser_version: '65.0'}.merge(sauce_w3c name)
+  when 'mac_mojave_safari'
+    {platform_name: 'macOS 10.14',
+     browser_name: 'safari',
+     browser_version: '12.0'}.merge(sauce_w3c(name))
+  when 'windows_7_ff'
+    {platform_name: 'Windows 7',
+     browser_name: 'firefox',
+     browser_version: '60.0'}.merge(sauce_w3c(name))
+  else
+    # Always specify a default;
+    # this doesn't force Chrome to w3c
+    {platform: 'macOS 10.12',
+     browser_name: 'chrome',
+     version: '65.0'}.merge(sauce_oss(name))
+  end
+end
+
+def sauce_w3c(name)
+  {'sauce:options' => {name: name,
+                       build: build_name,
+                       username: ENV['SAUCE_USERNAME'],
+                       access_key: ENV['SAUCE_ACCESS_KEY'],
+                       selenium_version: '3.141.59'}}
+end
+
+def sauce_oss(name)
+  {name: name,
+   build: build_name,
+   username: ENV['SAUCE_USERNAME'],
+   access_key: ENV['SAUCE_ACCESS_KEY'],
+   selenium_version: '3.141.59',
+  }
+end
+
+#
+# Note that this build name is specifically for Travis CI execution
+# Most CI tools have ENV variables that can be structured to provide useful build names
+#
+def build_name
+  if ENV['TRAVIS_REPO_SLUG']
+    "#{ENV['TRAVIS_REPO_SLUG'][/[^\/]+$/]}: #{ENV['TRAVIS_JOB_NUMBER']}"
+  elsif ENV['SAUCE_START_TIME']
+    ENV['SAUCE_START_TIME']
+  else
+    "Ruby-Cucumber-Capybara: Local-#{Time.now.to_i}"
+  end
 end
