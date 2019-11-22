fc77c2ef9fce4fd7de57819324cad41ba8e14bfc
James Tacker <james.tacker@saucelabs.com>
2019-11-22 12:48:14 -0800

    updated Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ruby:2.5
+COPY . .
+WORKDIR /demo-ruby
+CMD [""]
---
+++ setup.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+
+sudo apt-get update
+sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+rbenv install -l
+echo "gem: --no-document" > ~/.gemrc
+gem install bundler
+gem install rails
