16040b63d32aa1001a1f96139ddcfbfce24d031a
James Tacker <james.tacker@saucelabs.com>
2019-11-22 14:13:47 -0800

    updated config.yml

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 !#/bin/bash
 
 apt-get update
+#apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
