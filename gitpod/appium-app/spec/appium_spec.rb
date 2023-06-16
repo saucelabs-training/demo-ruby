2926faeed0f13958b613eb5209dd33481f1b9006
Dan Graham <dan@saucelabs.com>
2023-06-16 23:04:31 +0000

    appium specs

---
+++ gitpod/appium-app/spec/appium_spec.rb
@@ -0,0 +1,12 @@
+require 'selenium-webdriver'
+require 'rspec'
+require 'spec_helper'
+
+# see ./sepc_helper.rb for session creation and destruction
+describe 'Gitpod' do
+  it 'basic mobile test' do
+    # Add your actions and assertions here
+    # expect(@driver.page_source).to include('')
+    # @driver.get('https://www.example.com')
+  end
+end
