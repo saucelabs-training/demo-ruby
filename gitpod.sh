8cb1f3012b6ea5f89a14bb8fad979756fbf6e09a
Dan Graham <dan@saucelabs.com>
2023-06-16 21:33:09 +0000

    again

--- gitpod.sh
+++ gitpod.sh
@@ -3,7 +3,7 @@
 # Check if TEST_PATH is already set
 if [ -z ${TEST_PATH+x} ]; then
     # TEST_PATH is not set, assign the default value
-    TEST_PATH="/gitpod/desktop-browser"
+    TEST_PATH="./gitpod/desktop-browser"
 fi
 
 cd $TEST_PATH
